0b5a1dabe8c4b4781025002c7141d175c8a7a226
Richard Zeng <richard@ritual.co>
2019-07-06 17:28:39 -0400

    Set up react-router

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="App">
+            <h1>Hello World. Home</h1>
+        </div>
+    );
+}
+
+export default Home;
