68923ea7683142f49df16a28a9fdec079b60f5e7
Richard Zeng <richard@ritual.co>
2019-07-25 16:16:06 -0400

    Finish cover image

--- src/frontend/home.js
+++ src/frontend/home.js
@@ -5,23 +5,46 @@ import Paper from '@material-ui/core/Paper';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { withStyles } from '@material-ui/core/styles';
 import Image from 'material-ui-image'
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import image from "../public/background.png"
 
 const styles = theme => ({
-    mainFeaturedPost: {
-        position: 'relative',
-        backgroundColor: theme.palette.grey[800],
-        color: theme.palette.common.white,
-        marginBottom: theme.spacing(4),
-        backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-    },
     coverImage:{
         height:50,
     },
+    overlay: {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      backgroundColor: 'rgba(0,0,0,.3)',
+    },
+    mainFeaturedPostContent: {
+      position: 'relative',
+      padding: theme.spacing(3),
+      [theme.breakpoints.up('md')]: {
+        padding: theme.spacing(8),
+        paddingRight: 0,
+      },
+    },
+    coverImage:{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    coverImageParent:{
+        position:'relative',
+    },
+    coverImageText:{
+        color: "#FFFFFF"
+    }
 })
 
 class Home extends React.Component{
@@ -31,9 +54,26 @@ class Home extends React.Component{
             <div className="App">
             <Container maxWidth="lg">
                 <Navbar/>
-                    <LazyLoadImage
-                    src={image}
-                    width={"100%"}/>
+                    <div className={classes.coverImageParent}>
+                        <LazyLoadImage
+                        src={image}
+                        width={"100%"}
+                        height={"100%"}
+                        className={classes.coverImage}/>
+
+                        <Grid container>
+                          <Grid item md={6}>
+                            <div className={classes.mainFeaturedPostContent}>
+                              <Typography component="h1" variant="h3" className={classes.coverImageText} gutterBottom>
+                                Noob Programs Stuff
+                              </Typography>
+                              <Typography variant="h5" className={classes.coverImageText} paragraph>
+                                Watch me break stuff and learn the hard way so you don't have to
+                              </Typography>
+                            </div>
+                          </Grid>
+                        </Grid>
+                    </div>
                 <h1>hello</h1>
             </Container>
             </div>
