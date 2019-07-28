5df334374260b78988e7eca2fc298e009b79d92a
Richard Zeng <richard@ritual.co>
2019-07-28 16:23:50 -0400

    Add featured posts boxes

--- src/frontend/home.js
+++ src/frontend/home.js
@@ -10,8 +10,10 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Divider';
 import Markdown from "react-markdown";
+import Button from '@material-ui/core/Button';
 
 import image from "../public/background.png"
+import post from "../public/markdowns/home";
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const styles = theme => ({
@@ -53,6 +55,17 @@ const styles = theme => ({
     },
     textArea:{
         marginTop:25
+    },
+    featured:{
+        marginTop:25
+    },
+
+    featuredBoxes:{
+        paddingTop:25,
+        paddingBottom:25,
+        paddingLeft:15,
+        paddingRight:15,
+        textAlign:"left",
     }
 })
 
@@ -76,6 +89,7 @@ class Home extends React.Component{
                               <Typography component="h1" variant="h3" className={classes.coverImageText} gutterBottom>
                                 Noob Programs Stuff
                               </Typography>
+
                               <Typography variant="h5" className={classes.coverImageText} paragraph>
                                 Watch me break stuff and learn the hard way so you don't have to
                               </Typography>
@@ -84,15 +98,59 @@ class Home extends React.Component{
                         </Grid>
                     </div>
 
+                    <div className={classes.featured}>
+                        <Grid container spacing={3}>
+                            <Grid item sm={6}>
+                                <Paper className={classes.featuredBoxes}>
+                                    <Typography variant="h6" gutterBottom>
+                                        Featured Post
+                                    </Typography>
+
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                        July 2019
+                                    </Typography>
+
+                                    <Typography variant="subtitle1">
+                                        Let's talk about working in the booming tech space in Toronto
+                                    </Typography>
+
+                                    <Button color="primary">
+                                        Read More ...
+                                    </Button>
+                                </Paper>
+                            </Grid>
+
+                            <Grid item sm={6}>
+                                <Paper className={classes.featuredBoxes}>
+                                    <Typography variant="h6" gutterBottom>
+                                        Featured Post
+                                    </Typography>
+
+                                    <Typography variant="subtitle1" color="textSecondary">
+                                        Coming soon ...
+                                    </Typography>
+
+                                    <Typography variant="subtitle1">
+                                        Let's talk about working in the booming tech space in Toronto
+                                    </Typography>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </div>
+
                     <div>
                         <Grid container className={classes.textArea}>
                             <Grid item sm={2}></Grid>
                             <Grid item sm={8}>
-                                <Typography variant="h3" gutterBottom>
+                                <Typography variant="h4" gutterBottom>
                                     Just an Average Dude
                                 </Typography>
 
-                                <Markdown source={"Hello *World*!"}></Markdown>
+                                <Divider/>
+
+                                <Typography variant="body1" paragraph>
+                                    <Markdown source={post}></Markdown>
+                                </Typography>
 
                             </Grid>
                             <Grid item sm={2}></Grid>
---
+++ src/public/markdowns/home.js
@@ -0,0 +1,12 @@
+const homePost = `
+Have you ever hear
+
+"Its good to learn from your mistakes but its better to learn from the mistakes of others"
+
+
+Well be prepared to learn from an idiot. I'll make all the mistakes so you don't have to. If you read this and still make the mistakes, I guess you are the bigger idiot and should be running my blog. I'll try to cover things that most people want to try but just haven't had the opportunity to do it.
+
+I'll love to take suggestions about topics you want covered but just know that in order for me to have a well formed opinion, I have to try things out for multiple months.
+`
+
+export default homePost
