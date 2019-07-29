e4ee313c062ab106f76aa6cb88df8a8e8a3cd0de
Richard Zeng <richard@ritual.co>
2019-07-28 20:57:39 -0400

    WIP

--- src/frontend/about.js
+++ src/frontend/about.js
@@ -4,9 +4,23 @@ import { withStyles } from '@material-ui/core/styles';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import Markdown from "react-markdown";
+import Grid from '@material-ui/core/Grid';
+
+import post from '../public/markdowns/about';
 
 const styles = theme => ({
+    missionStatement:{
+        marginTop: "50px",
+        marginBottom: "50px"
+    },
 
+    textBlock:{
+        marginTop: "20px",
+        marginBottom: "20px"
+    }
 })
 
 class About extends React.Component{
@@ -16,7 +30,65 @@ class About extends React.Component{
             <div>
                 <Container maxWidth="lg">
                     <Navbar/>
-                    <h1>about</h1>
+
+                    <div className={classes.missionStatement}>
+
+                        <Grid container className={classes.textBlock}>
+                            <Grid item sm={3}></Grid>
+                            <Grid item sm={6}>
+                                <Typography variant="h4" gutterBottom>
+                                    - The Mission -
+                                </Typography>
+
+                                <Typography variant="h6" gutterBottom>
+                                    Learn from my mistakes
+                                </Typography>
+
+                                <Typography variant="body1" paragraph>
+                                    <Markdown source={post}></Markdown>
+                                </Typography>
+                            </Grid>
+                            <Grid item sm={3}></Grid>
+                        </Grid>
+
+                        <Divider/>
+
+                        <Grid container className={classes.textBlock} spacing={3}>
+                            <Grid item sm={6}>
+                                <LazyLoadImage
+                                src="https://cdn-2.returnofthecaferacers.com/wp-content/uploads/2018/03/bullitt-monster-821R-7-1-e1528087956742.jpg"
+                                width={"100%"}
+                                effect="blur"
+                                className={classes.coverImage}/>
+                            </Grid>
+
+                            <Grid item sm={6}>
+                            <Typography variant="h4" gutterBottom>
+                                Who Am I
+                            </Typography>
+
+                            <Typography variant="body1" gutterBottom>
+                                I "graduated" with a computer science degree from a well known university. Well I'm about to. I'm pretty much done. I was a full stack developer and a
+                                site reliability engineer. I've work for big companies, small companies and a university. I'm not your usual Silicon Valley developer and I hope
+                                you guys gain something of value from me.
+                            </Typography>
+                            </Grid>
+                        </Grid>
+
+                        <Divider/>
+
+                        <Grid container className={classes.textBlock} spacing={3}>
+
+                            <Grid item sm={2}></Grid>
+                            <Grid item sm={8}>
+                                <Typography variant="h4" gutterBottom>
+                                    Connect with me
+                                </Typography>
+                            </Grid>
+                            <Grid item sm={2}></Grid>
+
+                        </Grid>
+                    </div>
                 </Container>
 
                 <Footer/>
---
+++ src/public/markdowns/about.js
@@ -0,0 +1,8 @@
+const p = `
+I've gone through a lot of trial and error to get to where I currently am as a software engineer. Along the way, I've made many common mistakes that
+could have been avoided if someone informed me. Although I had mentors at the companies where I worked, I had minimal guidance in school, career progression
+and work life balance. I don't expect to be your mentor but just to be someone you can read about to see the possible outcomes of a decision before taking
+a leap of faith.
+`
+
+export default p
